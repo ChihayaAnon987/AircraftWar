908335d8277f702808a4f71742a78576ecce8163
ChihayaAnon987 <2048324682@qq.com>
2025-10-12 15:37:10 +0800

    lab3 finished

--- src/edu/hitsz/aircraft/SuperEliteEnemy.java
+++ src/edu/hitsz/aircraft/SuperEliteEnemy.java
@@ -3,26 +3,22 @@ package edu.hitsz.aircraft;
 import edu.hitsz.application.Main;
 import edu.hitsz.bullet.BaseBullet;
 import edu.hitsz.bullet.EnemyBullet;
-import edu.hitsz.bullet.HeroBullet;
 import edu.hitsz.prop.*;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 /**
- * 精英敌机
- * 可以射击
+ * 超级精英敌机
+ * 实现散射弹道
  *
  * @author ChihayaAnon
  */
-public class EliteEnemy extends AbstractAircraft {
-    /**攻击方式 */
-
+public class SuperEliteEnemy extends AbstractAircraft {
     /**
-     * 子弹一次发射数量
+     * 子弹发射数量
      */
-    private int shootNum = 1;
+    private int shootNum = 3;
 
     /**
      * 子弹伤害
@@ -39,7 +35,12 @@ public class EliteEnemy extends AbstractAircraft {
     private static final PropFactory fireSupplyFactory = new FireSupplyFactory();
     private static final PropFactory bombSupplyFactory = new BombSupplyFactory();
 
-    public EliteEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
+    /**
+     * 散射角度范围
+     */
+    private double spreadAngle = Math.PI / 6; // 30度
+
+    public SuperEliteEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
     }
 
@@ -47,29 +48,37 @@ public class EliteEnemy extends AbstractAircraft {
     public void forward() {
         super.forward();
         // 判定 y 轴向下飞行出界
-        if (locationY >= Main.WINDOW_HEIGHT ) {
+        if (locationY >= Main.WINDOW_HEIGHT) {
             vanish();
         }
     }
 
-    /**
-     * 通过射击产生子弹
-     * @return 射击出的子弹List
-     */
     @Override
     public List<BaseBullet> shoot() {
         List<BaseBullet> res = new LinkedList<>();
         int x = this.getLocationX();
-        int y = this.getLocationY() + direction*2;
-        int speedX = 0;
-        int speedY = this.getSpeedY() + direction*4;
+        int y = this.getLocationY() + direction * 2;
+
+        int baseSpeedX = this.speedX;
+        int baseSpeedY = this.getSpeedY();
+        int speedx;
+        int speedy = (direction > 0) ? (baseSpeedY + direction * 5) : (baseSpeedY + direction * 10);
         BaseBullet bullet;
-        for(int i=0; i<shootNum; i++){
-            // 子弹发射位置相对飞机位置向前偏移
-            // 多个子弹横向分散
-            bullet = new EnemyBullet(x + (i*2 - shootNum + 1)*10, y, speedX, speedY, power);
+
+        for (int i = 0; i < shootNum; i++) {
+            // 子弹发射位置相对飞机位置向前偏移，多颗子弹横向分散
+            // 横向速度分配：-2,0,2
+            if (i == 0) speedx = -2;
+            else if (i == 1) speedx = 0;
+            else speedx = 2;
+
+            int bx = x + (i * 2 - shootNum + 1) * 10;
+
+            // 敌机发射均为 EnemyBullet（向下或向上由 speedy 控制）
+            bullet = new EnemyBullet(bx, y, speedx + baseSpeedX, speedy, power);
             res.add(bullet);
         }
+
         return res;
     }
 
@@ -89,7 +98,7 @@ public class EliteEnemy extends AbstractAircraft {
     }
 
     /**
-     * 精英敌机坠毁后随机掉落道具
+     * 超级精英敌机坠毁后随机掉落道具
      * @return 掉落的道具，如果没有掉落则返回 null
      */
     public BaseProp dropProp() {
